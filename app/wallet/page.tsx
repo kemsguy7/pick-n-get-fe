ad864291459b90b1cb04bff3ccf82b674cccac4a
kemsguy7 <mattidungafa@gmail.com>
2025-09-18 07:26:31 +0100

    realigned styles of wallet icon on wallet page

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -5,6 +5,8 @@ import { useState } from "react"
 import AppLayout from "../components/layout/AppLayout"
 import WalletConnect from "../components/wallet/WalletConnect"
 import WalletConnected from "../components/wallet/WalletConnected"
+import { Wallet } from "lucide-react"
+
 
 export default function WalletPage() {
   const [isWalletConnected, setIsWalletConnected] = useState(false)
@@ -23,9 +25,17 @@ export default function WalletPage() {
         <div className=" mx-auto">
           {/* Header */}
           <div className="text-center mb-8 mt-4 lg:mb-12">
-            <h1 className="text-3xl md:text-4xl text-primary lg:text-5xl font-semibold text-white mb-4 font-space-grotesk">
-              Wallet 💰
-            </h1>
+            <div className="flex items-center justify-center gap-2 mb-4">
+                <div> 
+                    <h1 className="text-3xl md:text-4xl text-primary lg:text-5xl font-semibold text-white  font-space-grotesk">
+                    Wallet       
+                    </h1>
+            </div>
+                <div>
+                <Wallet className="w-10 h-10 text-green-500" />
+                </div>
+           </div>
+           
             <p className="text-lg text-gray-300 font-inter secondary-text font-medium">
               Manage your crypto assets and eco-tokens securely
             </p>
